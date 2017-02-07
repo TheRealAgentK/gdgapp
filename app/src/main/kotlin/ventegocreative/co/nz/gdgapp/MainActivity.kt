cb0063fefeb76c04762d25c08ddd1f8ac13f35da
Kai Koenig <kai@ventego-creative.co.nz>
2017-02-08 11:09:52 +1300

    Step 4 - Click handler with KAE and Lambdas

--- app/src/main/kotlin/ventegocreative/co/nz/gdgapp/MainActivity.kt
+++ app/src/main/kotlin/ventegocreative/co/nz/gdgapp/MainActivity.kt
@@ -2,6 +2,7 @@ package ventegocreative.co.nz.gdgapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         helloWorld.setText("Hello via KAE")
+
+        // plain Kotlin:
+        //
+        // findViewById(R.id.clickMe).setOnClickListener { view -> Log.d("MainActivity", "onClick: clickMe") }
+        // or
+        // findViewById(R.id.clickMe).setOnClickListener({ view -> Log.d("MainActivity", "onClick: clickMe")})
+
+        // with Kotlin Android Extensions:
+        //
+        clickMe.setOnClickListener { Log.d("MainActivity", "onClick: clickMe") }
     }
 }
